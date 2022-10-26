4dc374fe7b5d7f4d5f3a4164e36a996cde46cc52
Max <maxime@alkia.net>
2022-10-26 14:05:08 +0700

    Billing upgrade

--- scripts/custo/integration_tests/6-Billing.sh
+++ scripts/custo/integration_tests/6-Billing.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 electrad query meter currentcycle-id
+#### Prepare Bill ######################################################
+rm ~/electra/billing.log
+electrad query meter currentcycle-id
 electrad tx meter prepare-bill 78 false --from bob -y
+more ~/electra/billing.log
+#### Prepare Bill & Write in the blockchain ############################
+rm ~/electra/billing.log
+electrad query meter currentcycle-id
+electrad tx meter prepare-bill 78 true --from bob -y
 more ~/electra/billing.log
