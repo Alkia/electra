5f9a11164370933b1e3173d292faa70ea2755dc1
alkia <maxime@alkia.net>
2023-01-13 18:38:54 +0700

    App.vue change

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -8,7 +8,7 @@ const routerHistory = createWebHistory()
 const routes = [
   { path: '/', component: Portfolio },
   { path: '/portfolio', component: Portfolio },
- // { path: '/voter', component: Voter }
+  { path: '/voter', component: Voter },
   { path: '/data', component: Data }
 ]
 
