299122a2fc576922a8e56250f9528b5d23f0678b
alkia <maxime@alkia.net>
2023-01-13 14:48:08 +0700

    Create the file vue/src/views/Voter.vue that will contain our voting UI

---
+++ vue/src/views/Voter.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<div class="container">
+			<!-- this line is used by starport scaffolding # 4 -->
+		<h2>
+		Voter Module
+		</h2>
+		<PollForm />
+		<PollList />
+		</div>
+	</div>
+</template>
