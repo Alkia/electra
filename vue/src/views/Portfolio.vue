7f5ba27a1cb78d1871f740af255b050aebea5ad8
alkia <maxime@alkia.net>
2023-01-13 19:33:29 +0700

    App.vue change

--- vue/src/views/Portfolio.vue
+++ vue/src/views/Portfolio.vue
@@ -2,14 +2,14 @@
   <div class="container">
     <div class="row row-sm-revers">
       <div class="col-md-6">
-        <div class="row2">
+        <div class="row2 electra_sp">
           <SpAssets />
         </div>
         <br/>
-        <div class="row2"><SpTokenTransferList /></div>        
+        <div class="row2 electra_sp"><SpTokenTransferList /></div>        
       </div>
       <div class="col-md-5 col-lg-4 col-md-offset-1 col-lg-offset-2">
-        <div class="row2"><SpTokenTransfer /></div>
+        <div class="row2 electra_sp"><SpTokenTransfer /></div>
       </div>
     </div>
   </div>
@@ -56,4 +56,17 @@ export default {
   flex-grow: 1;
   padding: 20px;
 }
+</style>
+<style scoped>
+  .electra_sp {
+    border-radius: 1.2rem;
+    padding: 2rem;
+    font-size: 1.4rem;
+    background: rgba(234,220,255,0.3);
+    font-size: 2rem;
+    font-weight: 800;
+    letter-spacing: 0.03em;
+    margin-bottom: 2rem;
+    margin-bottom: 0.5rem;
+  }
 </style>
