ad0da021311b84a8ade6905ef4ad07e398fc3849
alkia <maxime@alkia.net>
2023-01-13 18:36:27 +0700

    App.vue change

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -40,6 +40,9 @@ export default {
     onBeforeMount(async () => {
       await $s.dispatch('common/env/init')
 
+      await $s.dispatch("electra.voter/QueryPollAll",{options:{subscribe:true, all:true},params:{}})
+      await $s.dispatch("electra.voter/QueryVoteAll",{options:{subscribe:true, all:true},params:{}})      
+
       router.push('portfolio')
     })
 
