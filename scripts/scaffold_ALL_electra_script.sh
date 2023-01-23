cec86b4c4c9e883fe58a104a60a29270eaa6a07f
Max <maxime@alkia.net>
2023-01-23 15:07:49 +0700

    Update scaffold_ALL_electra_script.sh

--- scripts/scaffold_ALL_electra_script.sh
+++ scripts/scaffold_ALL_electra_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2022-2023 Alkia <www.alkia.net>
-# All rights reserved.
+# All rights reserved. 
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
